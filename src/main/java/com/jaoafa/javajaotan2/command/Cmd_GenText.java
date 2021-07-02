95a6cdd4e13c3c5b643eed7f3d4c206e98e07a89
Tomachi <lego@aol.jp>
2021-07-03 06:20:30 +0900

    Update Cmd_GenText.java

--- src/main/java/com/jaoafa/javajaotan2/command/Cmd_GenText.java
+++ src/main/java/com/jaoafa/javajaotan2/command/Cmd_GenText.java
@@ -90,7 +90,9 @@ public class Cmd_GenText implements CommandPremise {
     }
 
     private String getSource(@NotNull CommandContext<JDACommandSender> context) {
-        if (!context.contains("count")) {
+        if (context.contains("count")) {
+            return context.get("sourceOrGenCount");
+        } else if (!JavajaotanLibrary.isInt(context.get("sourceOrGenCount"))) {
             return context.get("sourceOrGenCount");
         } else {
             return "default";

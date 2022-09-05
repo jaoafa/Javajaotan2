e9be0d83e4e4d418c8e0d00810f994d093025760
Tomachi <8929706+book000@users.noreply.github.com>
2022-09-05 21:40:24 +0900

    fix: えんえん絵文字のIDが違ったっぽい (#334)
    * fix: えんえん絵文字のIDが違ったっぽい
    
    * fix: remove system-out-print

--- src/main/java/com/jaoafa/javajaotan2/event/Event_AntiEnEn.java
+++ src/main/java/com/jaoafa/javajaotan2/event/Event_AntiEnEn.java
@@ -24,8 +24,8 @@ import java.util.List;
 
 public class Event_AntiEnEn extends ListenerAdapter {
     List<Long> targetEmojis = List.of(
-        1016289682763558972L, // :enen:
-        1016289682763558972L // :hin:
+        1001788329756475433L, // :enen:
+        1001788332939939901L // :hin:
     );
     long cryingRoomId = 858282427746877440L;
 
@@ -39,10 +39,12 @@ public class Event_AntiEnEn extends ListenerAdapter {
         if (member == null) return;
 
         // メッセージ内に該当絵文字が存在するか
+        // または「えんえん」「ひーん」単独で打ち込んだか
         Mentions mentions = event.getMessage().getMentions(); // getMentionsでカスタム絵文字を取得できる
         List<CustomEmoji> customEmojis = mentions.getCustomEmojis();
+        String content = event.getMessage().getContentRaw();
 
-        if (customEmojis.stream().noneMatch(e -> targetEmojis.contains(e.getIdLong()))) {
+        if (customEmojis.stream().noneMatch(e -> targetEmojis.contains(e.getIdLong())) && !content.equals("えんえん") && !content.equals("ひーん")) {
             return;
         }
 
